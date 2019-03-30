4038ef70aa7cdd4fb4d474b0f3b8ccd6d6d38535
colten <646043517@qq.com>
2019-03-30 14:04:08 +0800

    First commit.

---
+++ app/src/main/java/com/example/asus/coolweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.asus.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+https://github.com/Colten293/coolweather.git
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
